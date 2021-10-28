4c03435ebb6f2145d5e8ddc19592739605677a41
FaCoffee1984 <Francesco.Castellani@mottmac.com>
2021-10-28 13:01:52 +0100

    Created script

---
+++ scripts/3.dashboard.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+import streamlit as st
+import altair as alt
+from streamlit_folium import folium_static
+import folium
+from branca.colormap import linear, LinearColormap
