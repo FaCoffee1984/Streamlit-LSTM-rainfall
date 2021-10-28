63992f029aee9262273b0693129c6ff0e8a552e5
FaCoffee1984 <Francesco.Castellani@mottmac.com>
2021-10-28 13:17:00 +0100

    Fixed imports

--- scripts/3.dashboard.py
+++ scripts/3.dashboard.py
@@ -1,3 +1,5 @@
+'''Make sure you run pip install jinja2==2.11 before running these imports.'''
+
 import os
 import pandas as pd
 import streamlit as st
@@ -5,3 +7,4 @@ import altair as alt
 from streamlit_folium import folium_static
 import folium
 from branca.colormap import linear, LinearColormap
+
