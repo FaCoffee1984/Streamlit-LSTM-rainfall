a623767ad85f3c879dbc2b6c2cd312646a4523b2
FaCoffee1984 <Francesco.Castellani@mottmac.com>
2021-11-02 10:22:44 +0000

    Updated

--- dashboard.py
+++ dashboard.py
@@ -12,64 +12,65 @@ from branca.colormap import linear, LinearColormap
 import pydeck as pdk
 from dateutil.relativedelta import relativedelta # to add days or years
 import datetime as dt
+from utils import read_data_from_pickles, read_coordinates, add_time_slider
 
 
 
 #@st.cache  #Decorator caching data for faster runtimes
-def read_data_from_pickles(locations):
-    '''Read rainfall data and predictions from pickle file.'''
+# def read_data_from_pickles(locations):
+#     '''Read rainfall data and predictions from pickle file.'''
 
-    with open(r'C:\Users\CAS85405\python-dash-example\Dash-LSTM-rainfall\results\evaluation\eval.pkl', 'rb') as f:
-        data = pickle.load(f)
+#     with open(r'C:\Users\CAS85405\python-dash-example\Dash-LSTM-rainfall\results\evaluation\eval.pkl', 'rb') as f:
+#         data = pickle.load(f)
 
-    # Extract data for all locations
-    values = {}
+#     # Extract data for all locations
+#     values = {}
 
-    for location in locations:
-        predictions = data[location][0]
-        training_data = data[location][4]['rain_mm'].values
-        training_timeline = data[location][4]['timestamp'].values
-        prediction_timeline = data[location][5]['timestamp'].values
+#     for location in locations:
+#         predictions = data[location][0]
+#         training_data = data[location][4]['rain_mm'].values
+#         training_timeline = data[location][4]['timestamp'].values
+#         prediction_timeline = data[location][5]['timestamp'].values
 
-        # Combine timelines
-        timeline = np.concatenate([training_timeline, prediction_timeline])
+#         # Combine timelines
+#         timeline = np.concatenate([training_timeline, prediction_timeline])
 
-        # Aggregate past data
-        past = pd.DataFrame(index=range(0, len(training_timeline)))
-        past['date'] = training_timeline
-        past['rain (mm)'] = training_data
-        past['type'] = 'historic'
+#         # Aggregate past data
+#         past = pd.DataFrame(index=range(0, len(training_timeline)))
+#         past['date'] = training_timeline
+#         past['rain (mm)'] = training_data
+#         past['type'] = 'historic'
 
-        # Aggregate future data
-        future = pd.DataFrame(index=range(0, len(prediction_timeline)))
-        future['date'] = prediction_timeline
-        future['rain (mm)'] = predictions
-        future['type'] = 'predicted'
+#         # Aggregate future data
+#         future = pd.DataFrame(index=range(0, len(prediction_timeline)))
+#         future['date'] = prediction_timeline
+#         future['rain (mm)'] = predictions
+#         future['type'] = 'predicted'
 
-        to_plot = pd.concat([past,future], axis=0)
+#         to_plot = pd.concat([past,future], axis=0)
 
-        values[location] = to_plot
+#         values[location] = to_plot
 
-    return  values
+#     return  values
 
 
-@st.cache
-def read_coordinates():
-    '''Read coordinates from file.'''
+# @st.cache
+# def read_coordinates():
+#     '''Read coordinates from file.'''
 
-    base_file = pd.read_csv(r'C:\Users\CAS85405\python-dash-example\Dash-LSTM-rainfall\data\LOCATIONS.csv')
+#     base_file = pd.read_csv(r'C:\Users\CAS85405\python-dash-example\Dash-LSTM-rainfall\data\LOCATIONS.csv')
 
-    # Extract data for all locations
-    coordinates = {}
+#     # Extract data for all locations
+#     coordinates = {}
 
-    for index, row in base_file.iterrows():
-        location = row['Station']
-        latitude = row['Lat']
-        longitude = row['Lon']
+#     for index, row in base_file.iterrows():
+#         location = row['Station']
+#         latitude = row['Lat']
+#         longitude = row['Lon']
 
-        coordinates[location.lower()] = [latitude, longitude]
+#         coordinates[location.lower()] = [latitude, longitude]
 
-    return coordinates
+#     return coordinates
 
 
 @st.cache(allow_output_mutation=True)
@@ -220,29 +221,29 @@ def make_map2(data, lat, lon, zoom):
     return map2
 
 
-def add_time_slider(format, start_date_str, end_date_str):
-    '''Add time slider to Viz #2.
-       Provide start and end dates as strings: 'YYYY-MM-DD'.
-    '''
+# def add_time_slider(format, start_date_str, end_date_str):
+#     '''Add time slider to Viz #2.
+#        Provide start and end dates as strings: 'YYYY-MM-DD'.
+#     '''
 
-    # Dimensions
-    cols1,_ = st.columns((2,2)) #Increase second value to get a narrower slider
+#     # Dimensions
+#     cols1,_ = st.columns((2,2)) #Increase second value to get a narrower slider
 
-    # Define format. Available values can be 'MMM YYYY', 'DD MMM YYYY', 'MMM DD, YYYY'
-    format = format
+#     # Define format. Available values can be 'MMM YYYY', 'DD MMM YYYY', 'MMM DD, YYYY'
+#     format = format
 
-    # Define start and end dates
-    start_date = dt.date(year=int(start_date_str[0:4]), month=int(start_date_str[5:7]), day=int(start_date_str[8::]))
-    end_date = dt.date(year=int(end_date_str[0:4]), month=int(end_date_str[5:7]), day=int(end_date_str[8::]))
+#     # Define start and end dates
+#     start_date = dt.date(year=int(start_date_str[0:4]), month=int(start_date_str[5:7]), day=int(start_date_str[8::]))
+#     end_date = dt.date(year=int(end_date_str[0:4]), month=int(end_date_str[5:7]), day=int(end_date_str[8::]))
 
-    # Create slider and add it to map, returning the selected data
-    selected_date = st.slider(
-     'Select date',
-     min_value=start_date, max_value=end_date,
-     value=dt.date(2000,1,15), 
-     format=format)
+#     # Create slider and add it to map, returning the selected data
+#     selected_date = st.slider(
+#      'Select date',
+#      min_value=start_date, max_value=end_date,
+#      value=dt.date(2000,1,15), 
+#      format=format)
 
-    return selected_date
+#     return selected_date
 
 
 
@@ -273,7 +274,7 @@ This example uses monthly average rainfal data collected by the MetOffice for 6 
 - Manston
 - Oxford
 
-The structure of this app is shown here: https://github.com/FaCoffee1984/Dash-LSTM-rainfall
+The structure of this app is shown here: https://github.com/FaCoffee1984/Streamlit-LSTM-rainfall
 
 The data are freely available at: 
 https://www.metoffice.gov.uk/research/climate/maps-and-data/historic-station-data 
